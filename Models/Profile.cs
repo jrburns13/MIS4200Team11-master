53a6e760b5859ebaf4b01117155c961eba4ddafd
John Burns <jb818016@ohio.edu>
2021-03-08 15:28:10 -0500

    Add project files.

---
+++ Models/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team11.Models
+{
+    public class Profile
+    {
+        public Guid profileId { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string fullName
+        {
+            get
+            {
+                return lastName + ", " + firstName;
+            }
+        }
+        public string office { get; set; }
+        public string position { get; set; }
+        public DateTime hireDate { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200Team11.Startup))]
+namespace MIS4200Team11
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
