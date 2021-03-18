32f0bc5b683714b33cab7617b71eed6a3dddf945
Tanner Thomas <tt240016@ohio.edu>
2021-03-18 16:46:22 -0400

    Created DAL, added db for Profile, set up connection string

---
+++ DAL/Team11Context.cs
@@ -0,0 +1,18 @@
+﻿using MIS4200Team11.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team11.DAL
+{
+    public class Team11Context : DbContext
+    {
+        public Team11Context()  :   base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Profile> profile { get; set; }
+    }
+}
