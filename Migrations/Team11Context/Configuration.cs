b55dc18d6bea158e3035baba6bf332b03aa7ea60
Tanner Thomas <tt240016@ohio.edu>
2021-04-13 15:20:51 -0400

    Added Automatic Migrations

---
+++ Migrations/Team11Context/Configuration.cs
@@ -0,0 +1,24 @@
+namespace MIS4200Team11.Migrations.Team11Context
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MIS4200Team11.DAL.Team11Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"Migrations\Team11Context";
+        }
+
+        protected override void Seed(MIS4200Team11.DAL.Team11Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
